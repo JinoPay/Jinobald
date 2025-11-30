5925e2ac11aac437ad8e4ca3aa984f81159fe16f
박진호 <pjh3@paytalab.com>
2025-11-30 18:25:29 +0900

    디버그 빌드 아티팩트 업데이트 및 AssemblyInformationalVersion 변경.

--- src/Jinobald.Core/obj/Debug/net9.0/Jinobald.Core.AssemblyInfo.cs
+++ src/Jinobald.Core/obj/Debug/net9.0/Jinobald.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jinobald.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa35200e67a4e10c31e9519f9cb06586c2fb911e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8429d39a2e7fb930f957e1d80181c27ed4721aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jinobald.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jinobald.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
